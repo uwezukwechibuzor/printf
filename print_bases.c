b716cbbf9910f875dca599aeaf5f66c0a3ecb630
Chibuzor Daniel <uwezukwechibuzor@gmail.com>
2023-07-24 14:37:49 +0100

    Update print_bases.c

--- print_bases.c
+++ print_bases.c
@@ -1,4 +1,5 @@
-#include “main.h”
+#include "main.h"
+
 /**
  * print_hex - prints a number in hexadecimal base,
  * in lowercase
@@ -11,14 +12,16 @@
  */
 int print_hex(va_list l, flags_t *f)
 {
-unsigned int num = va_arg(l, unsigned int);
-char *str = convert(num, 16, 1);
-int count = 0;
-if (f->hash == 1 && str[0] != ‘0’)
-count += _puts(“0x”);
-count += _puts(str);
-return (count);
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 1);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0x");
+	count += _puts(str);
+	return (count);
 }
+
 /**
  * print_hex_big - prints a number in hexadecimal base,
  * in uppercase
@@ -31,14 +34,16 @@ return (count);
  */
 int print_hex_big(va_list l, flags_t *f)
 {
-unsigned int num = va_arg(l, unsigned int);
-char *str = convert(num, 16, 0);
-int count = 0;
-if (f->hash == 1 && str[0] != ‘0’)
-count += _puts(“0X”);
-count += _puts(str);
-return (count);
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0X");
+	count += _puts(str);
+	return (count);
 }
+
 /**
  * print_binary - prints a number in base 2
  * @l: va_list arguments from _printf
@@ -50,11 +55,13 @@ return (count);
  */
 int print_binary(va_list l, flags_t *f)
 {
-unsigned int num = va_arg(l, unsigned int);
-char *str = convert(num, 2, 0);
-(void)f;
-return (_puts(str));
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 2, 0);
+
+	(void)f;
+	return (_puts(str));
 }
+
 /**
  * print_octal - prints a number in base 8
  * @l: va_list arguments from _printf
@@ -66,11 +73,13 @@ return (_puts(str));
  */
 int print_octal(va_list l, flags_t *f)
 {
-unsigned int num = va_arg(l, unsigned int);
-char *str = convert(num, 8, 0);
-int count = 0;
-if (f->hash == 1 && str[0] != ‘0’)
-count += _putchar(‘0’);
-count += _puts(str);
-return (count);
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 8, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _putchar('0');
+	count += _puts(str);
+	return (count);
 }
+

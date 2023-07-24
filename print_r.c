2c308006372cb5c3f15248a7a21db8c053c088d1
Chibuzor Daniel <uwezukwechibuzor@gmail.com>
2023-07-24 14:36:17 +0100

    Update print_r.c

--- print_r.c
+++ print_r.c
@@ -1,4 +1,5 @@
-#include “main.h”
+#include "main.h"
+
 /**
  * print_bigS - Non printable characters
  * (0 < ASCII value < 32 or >= 127) are
@@ -11,28 +12,31 @@
  */
 int print_bigS(va_list l, flags_t *f)
 {
-int i, count = 0;
-char *res;
-char *s = va_arg(l, char *);
-(void)f;
-if (!s)
-return (_puts(“(null)“));
-for (i = 0; s[i]; i++)
-{
-if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
-{
-_puts(“\\x”);
-count += 2;
-res = convert(s[i], 16, 0);
-if (!res[1])
-count += _putchar(‘0’);
-count += _puts(res);
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		return (_puts("(null)"));
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			res = convert(s[i], 16, 0);
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
+		}
+		else
+			count += _putchar(s[i]);
+	}
+	return (count);
 }
-else
-count += _putchar(s[i]);
-}
-return (count);
-}
+
 /**
  * print_rev - prints a string in reverse
  * @l: argument from _printf
@@ -42,17 +46,22 @@ return (count);
  */
 int print_rev(va_list l, flags_t *f)
 {
-int i = 0, j;
-char *s = va_arg(l, char *);
-(void)f;
-if (!s)
-s = “(null)“;
-while (s[i])
-i++;
-for (j = i - 1; j >= 0; j--)
-_putchar(s[j]);
-return (i);
+	int i = 0, j;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		s = "(null)";
+
+	while (s[i])
+		i++;
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	return (i);
 }
+
 /**
  * print_rot13 - prints a string using rot13
  * @l: list of arguments from _printf
@@ -62,26 +71,29 @@ return (i);
  */
 int print_rot13(va_list l, flags_t *f)
 {
-int i, j;
-char rot13[] = “abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ”;
-char ROT13[] = “nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM”;
-char *s = va_arg(l, char *);
-(void)f;
-for (j = 0; s[j]; j++)
-{
-if (s[j] < ‘A’ || (s[j] > ‘Z’ && s[j] < ‘a’) || s[j] > ‘z’)
-_putchar(s[j]);
-else
-{
-for (i = 0; i <= 52; i++)
-{
-if (s[j] == rot13[i])
-_putchar(ROT13[i]);
+	int i, j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
+			{
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
+			}
+		}
+	}
+
+	return (j);
 }
-}
-}
-return (j);
-}
+
 /**
  * print_percent - prints a percent
  * @l: va_list arguments from _printf
@@ -90,7 +102,8 @@ return (j);
  */
 int print_percent(va_list l, flags_t *f)
 {
-(void)l;
-(void)f;
-return (_putchar(‘ % ’));
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
 }
+
